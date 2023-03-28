8162ed4dcd4e63bdf6562e32bb7ed7ce616b3160
Anto Alex <antoalex710@gmail.com>
2023-03-28 18:54:58 +0530

    cookie token created

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,10 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
-// const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const generateToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET, {expiresIn: "1d"});
+}
 
 const registerUser = asyncHandler( async (req, res) => {
     
@@ -37,21 +41,31 @@ const registerUser = asyncHandler( async (req, res) => {
         throw new Error("Account already exists for this mail id")
     }
 
-    //Encrypt password
-    // const salt = await bcrypt.genSalt(10);
-    // const hashedPassword = await bcrypt.hash(password, salt);
-
     //Create new User
     const user = await User.create({
         name,
         email,
         password,
-    })
+    });
+
+    //Generate Token
+    const token = generateToken(user._id); 
+
+    //send HTTP-only cookie
+    res.cookie("token", "token", {
+        path: "/",
+        httpOnly: true,
+        expires: new Date(Date.now() + 100 * 86400),
+        sameSite: "none",
+    });
+
+    console.log('Cookie set:', req.cookies);
+   // res.send("cookie send successfully");
 
     if(user){
         const {_id, name, email, photo, phone, bio} = user
         res.status(201).json({
-            _id, name, email, photo, phone, bio
+            _id, name, email, photo, phone, bio, token
         })
     } else {
         res.status(400);
