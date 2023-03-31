b979c7899aeebc5294bb407443b4c2fbf533d819
Anto Alex <antoalex710@gmail.com>
2023-04-01 03:16:09 +0530

    created a token for resetting password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,8 @@ const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
+const Token = require("../models/tokenModel");
+const crypto = require("crypto");
 
 const generateToken = (id) => {
     return jwt.sign({id}, process.env.JWT_SECRET, {expiresIn: "1d"});
@@ -183,10 +185,28 @@ const changePassword = asyncHandler(async (req, res) => {
         user.password = password;
         await user.save();
         res.status(200).send("Password changed successfully");
+    } else{
+        res.status(400);
+        throw new Error("Incorrect password please enter again");
     }
 });
 
 const forgotPassword = asyncHandler(async (req, res) => {
+    const {email} = req.body;
+    const user = await User.findOne({email});
+
+    if(!user){
+        res.status(404);
+        throw new Error("User not found");
+    }
+
+    //reset token
+    let resetToken = crypto.randomBytes(32).toString("hex") + user._id;
+
+    //hash token before saving
+    const hashedToken = crypto.createHash("sha256").update(resetToken).digest("hex");
+    console.log(hashedToken);
+    
     res.send("forgot password");
 });
 
