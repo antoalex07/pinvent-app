b82c7df9946239f39d79a8346c4be82e4a9cb4bc
Anto Alex <antoalex710@gmail.com>
2023-04-09 10:15:24 +0530

    setting the loading gifs

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
         <div className="--flex-between">
             <h3>
                 <span className="--fw-thin">Welcome, </span>
-                <span className="--color-danger">Anto Alex, </span>
+                <span className="--color-danger">Anto Alex </span>
             </h3>
             <button className="--btn --btn-danger">Logout</button>
         </div>
